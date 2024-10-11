6db623545d02fc44d694d07dfe40400d29a501a1
pstierle <p.stierle@web.de>
2024-10-12 00:59:17 +0200

    init

---
+++ lua/core.lua
@@ -0,0 +1,24 @@
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.opt.number = true
+vim.opt.showmode = false
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
---
+++ lua/plugins/toogleterm.lua
@@ -0,0 +1,10 @@
+return { 
+	{ 
+		"akinsho/toggleterm.nvim", 
+		config = true, 
+		cmd = "ToggleTerm", 
+		keys = { 
+			{ "<C-t>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" } 
+		}
+	}
+}
