ee47eb789270e846c77ed289775444cf10714b47
Stierle <paul.stierle@adesso-mobile.de>
2024-10-18 12:06:57 +0200

    added visual multi

--- lua/core.lua
+++ lua/core.lua
@@ -5,26 +5,33 @@ vim.cmd("set textwidth=0")
 vim.cmd("set expandtab")
 vim.cmd("set nowrap")
 
-vim.opt.number = true
-vim.opt.showmode = false
-vim.opt.cursorline = true
-
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+    vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
+-- VIM OPTS
+vim.opt.number = true
+vim.opt.showmode = false
+vim.opt.cursorline = true
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.updatetime = 250
 
+-- KEYMAPS
+
+-- undo
 vim.keymap.set({ "n", "v", "i" }, "<C-z>", "<Cmd>u<CR>", {})
+
+-- select all
+vim.keymap.set({ "n", "v" }, "<C-a>", "ggVG", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-a>", "<Esc>ggVG", { noremap = true, silent = true })
---
+++ lua/plugins/visual-multi.lua
@@ -0,0 +1,11 @@
+return {
+	"mg979/vim-visual-multi",
+	branch = "master",
+	init = function()
+		vim.keymap.set({ "n", "v", "i" }, "<C-d>", "<Nop>", { noremap = true, silent = true })
+		vim.g.VM_maps = {
+			["Find Under"] = "<C-d>",
+			["Find Subword Under"] = "<C-d>",
+		}
+	end,
+}
