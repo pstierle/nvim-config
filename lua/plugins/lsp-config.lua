b73eb436c16c622ee4c12fa75ea0e6b749341c22
Stierle <paul.stierle@adesso-mobile.de>
2024-10-30 20:57:57 +0100

    follow file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,49 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason.lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"gopls",
-					"jdtls",
-					"clangd",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.gopls.setup({})
-			lspconfig.clangd.setup({})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason.lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "ts_ls",
+                    "gopls",
+                    "jdtls",
+                    "clangd",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+            lspconfig.gopls.setup({})
+            lspconfig.clangd.setup({})
 
-			local jdtls_root = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
-			local lombok_path = jdtls_root .. "/lombok.jar"
+            local jdtls_root = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+            local lombok_path = jdtls_root .. "/lombok.jar"
 
-			lspconfig.jdtls.setup({
-				cmd = {
-					"jdtls",
-					"--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand(lombok_path)),
-				},
-			})
+            lspconfig.jdtls.setup({
+                cmd = {
+                    "jdtls",
+                    "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand(lombok_path)),
+                },
+            })
 
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set({ "n", "v", "i" }, "ch", "<Nop>", { noremap = true, silent = true })
+
+            vim.keymap.set("n", "ch", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "gf", vim.lsp.buf.format, {})
+        end,
+    },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,24 +1,27 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		require("neo-tree").setup({
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					show_hidden_count = true,
-					hide_dotfiles = false,
-					hide_gitignored = false,
-					hide_by_name = {},
-					never_show = {},
-				},
-			},
-		})
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_by_name = {},
+                    never_show = {},
+                },
+                follow_current_file = {
+                    enabled = true
+                }
+            },
+        })
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+    end,
 }
