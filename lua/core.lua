c5f6948eea3aaf0be2531ad3d892af30017abc88
Stierle <paul.stierle@adesso-mobile.de>
2024-10-31 10:22:41 +0100

    fix typo

--- lua/core.lua
+++ lua/core.lua
@@ -2,15 +2,15 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+        vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+        desc = "Highlight when yanking (copying) text",
+        group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+        callback = function()
+                vim.highlight.on_yank()
+        end,
 })
 
 -- VIM OPTS
@@ -25,7 +25,7 @@ vim.opt.updatetime = 250
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
-vim.o.shiftwifth = 4
+vim.o.shiftwidth = 4
 
 -- KEYMAPS
 
